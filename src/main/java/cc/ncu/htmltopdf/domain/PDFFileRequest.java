b95cdd8c45f1601330e6a2884521dfaf1b48b7b5
Yeh-Yung <yyc1217@gmail.com>
2016-01-05 17:05:01 +0000

    add Orientation

--- src/main/java/cc/ncu/htmltopdf/domain/PDFFileRequest.java
+++ src/main/java/cc/ncu/htmltopdf/domain/PDFFileRequest.java
@@ -1,47 +1,70 @@
 package cc.ncu.htmltopdf.domain;
 
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+import static org.springframework.util.StringUtils.getFilenameExtension;
+import static org.springframework.util.StringUtils.hasText;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.util.StringUtils;
+import cc.ncu.htmltopdf.enums.Orientation;
 
 public class PDFFileRequest {
-	
+
     private static final String DEFAULT_FILENAME = "result.pdf";
-    
-	@NotNull
-	private String target;
-	
-	@Size(min=1)
-	private String filename;
 
-	public PDFFileRequest() {
-		this.filename = DEFAULT_FILENAME;
-	}
+    @NotNull
+    private String target;
 
-	public String getTarget() {
-		return target;
-	}
+    @Size(min = 1)
+    private String filename;
 
-	public void setTarget(String target) {
-		this.target = target;
-	}
+    private Orientation orientation;
 
-	public String getFilename() {
-		return filename;
-	}
+    public PDFFileRequest() {
+        this.filename = DEFAULT_FILENAME;
+        this.orientation = Orientation.PORTRAIT;
+    }
 
-	public void setFilename(String filename) {
-		
-		this.filename = filename;
-		
-		if (!StringUtils.hasText(filename)) {
-		    this.filename = DEFAULT_FILENAME;
-		}
-		
-		String extension = StringUtils.getFilenameExtension(this.filename);
-		if (!"pdf".equalsIgnoreCase(extension)) {
-			this.filename += ".pdf";
-		}
-	}
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+
+        this.filename = filename;
+
+        if (!hasText(filename)) {
+            this.filename = DEFAULT_FILENAME;
+        }
+
+        String extension = getFilenameExtension(this.filename);
+        if (!"pdf".equalsIgnoreCase(extension)) {
+            this.filename += ".pdf";
+        }
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        for (Orientation orienta : Orientation.values()) {
+            if (equalsIgnoreCase(orienta.name(), orientation)) {
+                this.orientation = orienta;
+            }
+        }
+    }
 }
---
+++ src/main/java/cc/ncu/htmltopdf/enums/Orientation.java
@@ -0,0 +1,5 @@
+package cc.ncu.htmltopdf.enums;
+
+public enum Orientation {
+    LANDSCAPE, PORTRAIT
+}
