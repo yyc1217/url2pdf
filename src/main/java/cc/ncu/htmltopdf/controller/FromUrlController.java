47db782c5b3c54fd31b840ca4ee77d84ac5da09a
Yeh-Yung <yyc1217@gmail.com>
2016-01-05 15:26:14 +0000

    url convert to PDF ok

--- src/main/java/cc/ncu/htmltopdf/controller/FromUrlController.java
+++ src/main/java/cc/ncu/htmltopdf/controller/FromUrlController.java
@@ -1,11 +1,13 @@
 package cc.ncu.htmltopdf.controller;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cc.ncu.htmltopdf.domain.PDFFileRequest;
@@ -18,7 +20,13 @@ public class FromUrlController {
 	private IPdfConverter pdfConverter;
 
 	@RequestMapping(value = "/fromUrl", method = RequestMethod.GET)
-	void createPdf(HttpServletResponse response, @RequestParam PDFFileRequest fileRequest) {
-		pdfConverter.writePdfToResponse(fileRequest, response);
+	void createPdf(HttpServletResponse response, @Valid @ModelAttribute PDFFileRequest fileRequest, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			// TODO show error messages
+			throw new RuntimeException(result.toString());
+		}
+		
+		this.pdfConverter.writePdfToResponse(fileRequest, response);
 	}
 }
--- src/main/java/cc/ncu/htmltopdf/service/impl/PdfConverter.java
+++ src/main/java/cc/ncu/htmltopdf/service/impl/PdfConverter.java
@@ -1,9 +1,7 @@
 package cc.ncu.htmltopdf.service.impl;
 
-import static java.util.Objects.requireNonNull;
 import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.util.Assert.hasText;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,23 +15,24 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import cc.ncu.htmltopdf.domain.PDFFileRequest;
 import cc.ncu.htmltopdf.service.IPdfConverter;
 
+@Service
 public class PdfConverter implements IPdfConverter {
 
 	private static final Logger logger = LoggerFactory.getLogger(PdfConverter.class);
+	
+	@Value("${wait.seconds}")
+	private Integer waitSeconds;
 
 	@Override
 	public void writePdfToResponse(PDFFileRequest fileRequest, HttpServletResponse response) {
-		String pdfFileName = fileRequest.getFilename();
-		requireNonNull(pdfFileName, "The file name of the created PDF must be set");
-		hasText(pdfFileName, "File name of the created PDF cannot be empty or be blank");
-
+		
 		String targetUrl = fileRequest.getTarget();
-		requireNonNull(targetUrl, "Target HTML url must be set");
-		hasText(targetUrl, "Target HTML url cannot be empty");
 
 		List<String> pdfCommand = Arrays.asList("wkhtmltopdf", targetUrl, "-");
 
@@ -44,10 +43,10 @@ public class PdfConverter implements IPdfConverter {
 			pdfProcess = pb.start();
 
 			try (InputStream in = pdfProcess.getInputStream()) {
+				setResponseHeaders(response, fileRequest);
 				writeCreatedPdfFileToResponse(in, response);
 				waitForProcessBeforeContinueCurrentThread(pdfProcess);
 				requireSuccessfulExitStatus(pdfProcess);
-				setResponseHeaders(response, fileRequest);
 			} catch (Exception ex) {
 				writeErrorMessageToLog(ex, pdfProcess);
 				throw new RuntimeException("PDF generation failed");
@@ -62,12 +61,11 @@ public class PdfConverter implements IPdfConverter {
 	private void writeCreatedPdfFileToResponse(InputStream in, HttpServletResponse response) throws IOException {
 		OutputStream out = response.getOutputStream();
 		IOUtils.copy(in, out);
-		out.flush();
 	}
 
 	private void waitForProcessBeforeContinueCurrentThread(Process process) {
 		try {
-			process.waitFor(5, TimeUnit.SECONDS);
+			process.waitFor(waitSeconds, TimeUnit.SECONDS);
 		} catch (InterruptedException ex) {
 			Thread.currentThread().interrupt();
 		}
